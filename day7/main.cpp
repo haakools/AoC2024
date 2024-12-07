7a69c462ba8206a12ce0e606885ad9dd3b12f69b
Håkon Berggren Olsen <hakon.berggren.olsen@semcon.com>
2024-12-07 13:15:47 +0100

    blargh

--- day7/main.cpp
+++ day7/main.cpp
@@ -22,7 +22,6 @@ std::vector<std::string> readFileLines(const std::string& filename) {
 }
 
 
-
 std::vector<int> findAllArguments(std::string line) {
 
     std::vector<int> arguments;
@@ -31,7 +30,7 @@ std::vector<int> findAllArguments(std::string line) {
     int prevPos = 0;
     int pos = line.find(" ", 0);
 
-    
+
     while ( pos != std::string::npos ) {
         prevPos = pos;
         pos = line.find(" ", prevPos+1);
@@ -41,41 +40,152 @@ std::vector<int> findAllArguments(std::string line) {
                     )
                 );
     };
-
-
     return arguments;
-
 }
+
+
+int calculateOutput(std::vector<int> argVec, std::string operators ) {
+
+    // Now need to do a tree search if the ops "+" and "*" can solve the solution
+    int solution = 0; // working solution to compare with target
+    for (int i = 0; i < argVec.size()-1; i++) { 
+        char op = operators[i];
+        switch (op) {
+            case '+':
+                solution = argVec[i] + argVec[i+1];
+                break;
+            case '*':
+                solution = argVec[i] * argVec[i+1];
+                break;
+            default:
+                std::cout << "Default statement: " << std::endl;
+                std::cout << op << std::endl;
+                break;
+
+        };
+    }
+    return solution;
+}
+
+
+std::vector<std::string> generateOperators(const std::vector<std::string>& operators, size_t length) {
+    std::vector<std::string> result;
+    
+    // Recursive case
+    std::vector<std::string> subResults = generateOperators(operators, length - 1);
+    
+    // For each previous result, append each possible operator
+    for (const std::string& subResult : subResults) {
+        for (const std::string& op : operators) {
+            result.push_back(subResult + op);
+        }
+    }
+    
+    return result;
+}
+
+/*
+std::vector<std::string> generateOperators(const std::vector<std::string> operatorList) {
+    std::vector<std::string> result;
+    // I am a genius
+    for(const std::string val1 : operatorList) {
+        for(const std::string val2 : operatorList) {
+            for(const std::string val3 : operatorList) {
+                for(const std::string val4 : operatorList) {
+                    for(const std::string val5 : operatorList) {
+                        for(const std::string val6 : operatorList) {
+                            for(const std::string val7 : operatorList) {
+                                for(const std::string val8 : operatorList) {
+                                    for(const std::string val9 : operatorList) {
+                                        for(const std::string val10 : operatorList) {
+                                            for(const std::string val11 : operatorList) {
+                                                for(const std::string val12 : operatorList) {
+                                                    for(const std::string val13 : operatorList) {
+                                                        std::string combination;
+                                                        combination += val1;
+                                                        combination += val2;
+                                                        combination += val3;
+                                                        combination += val4;
+                                                        combination += val5;
+                                                        combination += val6;
+                                                        combination += val7;
+                                                        combination += val8;
+                                                        combination += val9;
+                                                        combination += val10;
+                                                        combination += val11;
+                                                        combination += val12;
+                                                        combination += val13;
+                                                        result.push_back(combination);
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return result;
+}
+*/
+
+
 
 
 int solve_puzzle_1(std::vector<std::string> lines) {
 
+    int calibrationSum = 0;
 
     for (int i=0; i< lines.size(); i++) {
-        std::cout << "Parsing line: " << std::endl;
-        std::cout << lines[i] <<  std::endl;
+        // Parsing inputs
+        // std::cout << "Parsing line: " << std::endl;
+        // std::cout << lines[i] <<  std::endl;
 
-        // parse into result and then numbers
+        // parse into target and then numbers
         int semicolonIdx = lines[i].find(":");
-        int result = std::stoi(lines[i].substr(0, semicolonIdx));
+        int target = std::stoi(lines[i].substr(0, semicolonIdx));
 
-        std::cout << "Target result: " << std::endl;
-        std::cout << result << std::endl;
-
+        // std::cout << "Target target: " << std::endl;
+        // std::cout << target << std::endl;
 
         std::string arguemntsString = lines[i].substr(semicolonIdx+1, std::string::npos);
-        std::vector<int> arguments = findAllArguments(arguemntsString);
+        std::vector<int> argVec = findAllArguments(arguemntsString);
 
-        std::cout << "Arguments: " << std::endl;
+        //std::cout << "Arguments: " << std::endl;
         // first find all spaces 
-        for (int i = 0; i<arguments.size(); i++) {
-            std::cout << arguments[i] << std::endl;
+        //for (int i = 0; i<argVec.size(); i++) {
+        //    std::cout << argVec[i] << std::endl;
+        //}
 
+        // hopefully a vector of all solutions
+        std::vector<std::string> operatorSolutions = generateOperators({"*", "+"}, argVec.size());
+
+        for (int i = 0; i < operatorSolutions.size(); i++) {
+            std::string operatorSolution = operatorSolutions[i]; //.substr(0, argVec.size());
+
+            std::cout << operatorSolution << std::endl;
+
+            // test solution
+            int possibleSolution = calculateOutput(argVec, operatorSolution);
+            if (possibleSolution == target) {
+                std::cout << "Found solution: " << std::endl;
+                std::cout << operatorSolution << std::endl;
+                calibrationSum = calibrationSum + target;
+                break;
+            }
         }
+    }
 
-    };
+    std::cout << "Calibration Result Sum: " << std::endl;
+    std::cout << calibrationSum << std::endl;
+
     return 0;
-}
+
+};
 
 int main() {
     try {
